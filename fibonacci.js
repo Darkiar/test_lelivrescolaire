142cfc4574589214b57950fdc5e07dffa8adaa5e
Adem ORDEK <aordek@localhost.localdomain>
2021-07-13 10:44:21 +0200

    modif text color fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -3,7 +3,6 @@ let n1 = 0, n2 = 1;
 function display() {
     let n3 = 0;
     let fibonacci = document.querySelector(".fibonacci");
-    let rgbHue = 255 / 99;
 
     fibonacci.innerHTML = "";
     for (let i = 0; i < 100; i++) {
@@ -12,9 +11,7 @@ function display() {
         n1 = n2;
         n2 = n3;
         div.innerHTML = n1;
-        div.style.color = `rgb(${rgbHue}, ${rgbHue}, ${rgbHue})`;
         fibonacci.appendChild(div)
-        rgbHue += 255 / 99;
     }
 }
 
